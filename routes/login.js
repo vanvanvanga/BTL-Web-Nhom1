a9ceab2e53aa887f7389c70aab5e80ff879c6e03
no name is permanent <98455140+iUseTooMuchInternet@users.noreply.github.com>
2025-05-31 15:23:16 +0700

    Thêm tính năng đăng nhập

--- routes/login.js
+++ routes/login.js
@@ -19,33 +19,63 @@ router.post("/", (req, res) => {
   let query = `
     SELECT * FROM users
     WHERE username = "${username}"
-    AND password = "${password}"
   `;
 
   // If username and password is not null
-  connection.query(query, (err, results) => {
-    if (results.length) {
-      res.send(`Xin chào, ${results[0].username}!`);
-    } else {
-      res.send("Tên người dùng hoặc mật khẩu không chính xác.");
-    }
+  if (username && password) {
+    connection.query(query, (err, results) => {
+      if (err) return res.status(500).send();
+      if (results.length == 0) {
+        return res.render("pages/login", {
+          message: "Người dùng không tồn tại hoặc mật khẩu không đúng!"
+        })
+      }
+      bcrypt.compare(password, results[0].password).then(
+        (result) => {
+          if (result) {
+            return res.render("pages/login", {
+              message: "Đăng nhập thành công!"
+            })
+          } else {
+            return res.render("pages/login", {
+              message: "Người dùng không tồn tại hoặc mật khẩu không đúng!"
+            })
+          }
+        }
+      )
 
-    if (err) {
-      console.log(err);
-    }
-  });
+      // connection.query(query, (err, results) => {
+      //   if (results.length) {
+      //     res.send(`Xin chào, ${results[0].username}!`);
+      //   } else {
+      //     res.send("Tên người dùng hoặc mật khẩu không chính xác.");
+      //   }
+    
+      //   if (err) {
+      //     console.log(err);
+      //   }
+    })
+  } else {
+    res.render("pages/login", {
+      message: "Tên người dùng và mật khẩu không được bỏ trống."
+    })
+  }
 });
 
 // Đăng ký
 router.get("/register", (req, res) => {
-  res.render("pages/register");
+  res.render("pages/register", {
+    message: null
+  });
 });
 
 router.post("/register", (req, res) => {
   let { username, password, confirmPassword } = req.body;
 
   if (password != confirmPassword) {
-    return res.send("Lỗi: Mật khẩu được nhập lại không đúng.");
+    return res.render("pages/register", {
+      message: "Lỗi: Mật khẩu được nhập lại không đúng."
+  });
   }
 
   let query = `
@@ -56,9 +86,13 @@ router.post("/register", (req, res) => {
   connection.query(query, (err, results) => {
     if (err) {
       console.log(err);
-      res.sendStatus(500);
+      return res.render("pages/register", {
+        message: "Có lỗi đã xảy ra, vui lòng thử lại..."
+    });
     } else if (results.length > 0) {
-      return res.send("Lỗi: Tên người dùng đã được sử dụng.");
+      return res.render("pages/register", {
+        message: "Lỗi: Tên người dùng đã được sử dụng."
+    });
     }
   });
 
@@ -74,9 +108,12 @@ router.post("/register", (req, res) => {
         `,
       (err, results) => {
         if (err) return res.send(err);
-        res.send("Đăng ký thành công!");
+        res.render("pages/register", {
+          message: "Đăng ký thành công! Mời bạn đăng nhập."
+        });
       }
     );
   });
 });
+
 module.exports = router;
